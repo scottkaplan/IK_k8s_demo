251d66daf565ae83ae25df554ceaf42957419200
Scott Kaplan <scott@kaplans.com>
2024-02-29 17:39:09 -0800

    Restructure into on subdir per sub demo.

--- terraform/1-net.tf
+++ terraform/1-net.tf
@@ -27,33 +27,9 @@ resource "aws_internet_gateway" "IK-igw" {
   }
 }
 
-resource "aws_subnet" "private-us-west-1a" {
-  vpc_id            = aws_vpc.IK.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-west-1a"
-
-  tags = {
-    "Name"                            = "private-us-west-1a"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
-  }
-}
-
-resource "aws_subnet" "private-us-west-1c" {
-  vpc_id            = aws_vpc.IK.id
-  cidr_block        = "10.0.32.0/19"
-  availability_zone = "us-west-1c"
-
-  tags = {
-    "Name"                            = "private-us-west-1c"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
-  }
-}
-
 resource "aws_subnet" "public-us-west-1a" {
   vpc_id                  = aws_vpc.IK.id
-  cidr_block              = "10.0.64.0/19"
+  cidr_block              = "10.0.0.0/19"
   availability_zone       = "us-west-1a"
   map_public_ip_on_launch = true
 
@@ -74,51 +50,6 @@ resource "aws_subnet" "public-us-west-1c" {
     "Name"                       = "public-us-west-1c"
     "kubernetes.io/role/elb"     = "1"
     "kubernetes.io/cluster/demo" = "owned"
-  }
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-
-  tags = {
-    Name = "nat"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-us-west-1a.id
-
-  tags = {
-    Name = "nat"
-  }
-
-  depends_on = [aws_internet_gateway.IK-igw]
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.IK.id
-
-  route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-    },
-  ]
-
-  tags = {
-    Name = "private"
   }
 }
 
@@ -148,16 +79,6 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "private-us-west-1a" {
-  subnet_id      = aws_subnet.private-us-west-1a.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private-us-west-1c" {
-  subnet_id      = aws_subnet.private-us-west-1c.id
-  route_table_id = aws_route_table.private.id
-}
-
 resource "aws_route_table_association" "public-us-west-1a" {
   subnet_id      = aws_subnet.public-us-west-1a.id
   route_table_id = aws_route_table.public.id
@@ -166,49 +87,4 @@ resource "aws_route_table_association" "public-us-west-1a" {
 resource "aws_route_table_association" "public-us-west-1c" {
   subnet_id      = aws_subnet.public-us-west-1c.id
   route_table_id = aws_route_table.public.id
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.demo.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.demo.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
-  }
-}
-
-resource "kubernetes_service" "demo" {
-  metadata {
-    name = "demo"
-    annotations = {
-      "service.beta.kubernetes.io/aws-load-balancer-name" = "demo"
-      "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb"
-      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "instance"
-      "service.beta.kubernetes.io/load-balancer-source-ranges" = "0.0.0.0/0"
-      "service.beta.kubernetes.io/aws-load-balancer-scheme": "internet-facing"
-    }
-  }
-  spec {
-    selector = {
-      app = "web"
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
-    type = "LoadBalancer"
-  }
-}
-
-resource "aws_route53_record" "ik-k8s" {
-  zone_id = data.aws_route53_zone.kaplans.zone_id
-  name    = "ik-k8s.kaplans.com"
-  type    = "CNAME"
-  ttl     = 300
-  records = [kubernetes_service.demo.status.0.load_balancer.0.ingress.0.hostname]
-}
-
-output "load_balancer_hostname" {
-  value = kubernetes_service.demo.status.0.load_balancer.0.ingress.0.hostname
 }
--- terraform/2-eks.tf
+++ terraform/2-eks.tf
@@ -35,8 +35,6 @@ resource "aws_eks_cluster" "demo" {
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.private-us-west-1a.id,
-      aws_subnet.private-us-west-1c.id,
       aws_subnet.public-us-west-1a.id,
       aws_subnet.public-us-west-1c.id
     ]
@@ -75,21 +73,21 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadO
   role       = aws_iam_role.nodes.name
 }
 
-resource "aws_eks_node_group" "private-nodes" {
+resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "private-nodes"
+  node_group_name = "nodes"
   node_role_arn   = aws_iam_role.nodes.arn
 
   subnet_ids = [
-    aws_subnet.private-us-west-1a.id,
-    aws_subnet.private-us-west-1c.id
+    aws_subnet.public-us-west-1a.id,
+    aws_subnet.public-us-west-1c.id
   ]
 
   capacity_type  = "ON_DEMAND"
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     max_size     = 5
     min_size     = 0
   }
