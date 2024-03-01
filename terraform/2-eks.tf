a8fba240299d162ab6ad66b839d195b26f0fb467
Scott Kaplan <scott@kaplans.com>
2024-03-01 12:46:25 -0800

    Changes after first dry run.

--- terraform/2-eks.tf
+++ terraform/2-eks.tf
@@ -87,7 +87,7 @@ resource "aws_eks_node_group" "nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 5
     min_size     = 0
   }
